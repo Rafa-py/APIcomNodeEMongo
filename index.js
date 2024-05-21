632b00670715b3c8979d1501b2cfd6f410f73619
Rafa-py <rafadevjs@gmail.com>
2024-05-21 18:19:10 -0300

    Create CRUD

--- index.js
+++ index.js
@@ -1,26 +1,121 @@
-//index.js 
-//configuração inicial 
-const express = require('express');
-const mongoose = require('mongoose');
+// config inicial
+const express = require('express')
+const app = express()
 
-const app = express();
+// depois do db
+const mongoose = require('mongoose')
 
-//Permitindo o express ler JSON 
-//middlewares =  é a maneira de fazer alguma coisa antes da requisição ser processada
+const Person = require('./models/Person')
+
 app.use(
-	express.urlencoded({
-		extended: true,
-	}),
+  express.urlencoded({
+    extended: true,
+  }),
 )
+
 app.use(express.json())
 
-//Rota inical / endpoint
-app.get('/', (req, res)=>{
-	//Mostrar req
-	res.json({message: "Olá, mundo!"})
+// Rotas
+app.post('/person', async (req, res) => {
+  const { name, salary, approved } = req.body
+
+  const person = {
+    name,
+    salary,
+    approved,
+  }
+  // O C do CRUD
+  try {
+    await Person.create(person)
+
+    res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
 })
 
-// Permitindo a visualização do express ao navegador postman 
+
+// O R do CRUD 
+app.get('/person', async (req, res) => {
+  try {
+    const people = await Person.find()
+
+    res.status(200).json(people)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+
+// O R do CRUD 
+app.get('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const person = await Person.findOne({ _id: id })
+
+    if (!person) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+// O U do CRUD 
+app.patch('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  const { name, salary, approved } = req.body
+
+  const person = {
+    name,
+    salary,
+    approved,
+  }
+
+  try {
+    const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+    if (updatedPerson.matchedCount === 0) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+// O D do CRUD 
+app.delete('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  const person = await Person.findOne({ _id: id })
+
+  if (!person) {
+    res.status(422).json({ message: 'Usuário não encontrado!' })
+    return
+  }
+
+  try {
+    await Person.deleteOne({ _id: id })
+
+    res.status(200).json({ message: 'Usuário removido com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Oi Express!' })
+})
+
+// Conectando ao banco
 let senha = "123456teste"
 mongoose.connect(`mongodb+srv://rafadev:${senha}@cluster0.vtsrted.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
     console.log("Conectamos ao mongoDB")
